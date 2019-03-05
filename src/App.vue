a98e112fd10e413470ad78732a2d0ae7d7c6b444
Adrien Carpentier <moi@adriencarpentier.com>
2019-03-05 23:15:18 +0100

    Few Vue tryouts

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
 	<div id="app">
-		<!-- <img alt="Vue logo" src="./assets/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
-		<div id="frise">
-		</div>
+		<!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+		<div id="frise"></div>
 	</div>
 </template>
 
--- src/components/Frise.vue
+++ src/components/Frise.vue
@@ -1,32 +1,27 @@
 <template>
-
+	<div>FRISE CHARGEE</div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import json from '../src/frise_periodes.json';
+// import json from '../src/frise_periodes.json';
 // import json from '../json/frise_evenements.json';
 
 @Component
 export default class Frise extends Vue {
-	// console.log(json);
+
+	// initial data
+	msg = 123
+
+	// lifecycle hook
+	mounted () {
+		this.greet()
+	}
+
+	// method
+	greet () {
+		alert('greeting: ' + this.msg)
+	}
+
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-	margin: 40px 0 0;
-}
-ul {
-	list-style-type: none;
-	padding: 0;
-}
-li {
-	display: inline-block;
-	margin: 0 10px;
-}
-a {
-	color: #42b983;
-}
-</style>
