d21c2dffc11f40ce60d1d06210f8f700b755536d
Adrien Carpentier <me@adriencarpentier.com>
2025-01-23 15:29:40 +0900

    chore: use tailwindcss 4 and tailwindcss/vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from "@sveltejs/kit/vite"
 import { defineConfig } from "vite"
+import packageJson from "./package.json" with { type: "json" }
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
+	define: {
+		__APP_VERSION__: JSON.stringify(packageJson.version),
+	}
 })
